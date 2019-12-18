849e0e5933040fb1b6f3d2123c37370f1a213d8f
Amy Franz <amy.franz@gmail.com>
2019-12-18 21:06:50 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,9 +1,11 @@
+$status
+
 def mario
   phrase = "It's-a me, Mario!"
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase
 end
 
-def toadstool
+def toadstool (status)
   puts status
 end
